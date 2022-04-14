5861db2fc62505c445f68c24cb4e4217bb59b121
Oliver Doll <oliver.doll@tu-ilmenau.de>
2022-04-14 13:21:14 +0200

    added openMHA config skript

--- work.py
+++ work.py
@@ -2,9 +2,11 @@ import soundfile as sf
 import numpy as np
 import logging
 import ast
+import scipy.io
 
-x = [1, 'bin', 2, 'bin']
-x_ = [i for i in x if i != 'bin']
-print(len(x_))
-print(len(set(x_)))
-print(set(x_))
+path_to_mat = "example/brirs/BRIRs_ListeningLab_2m_LS-Front/brirs125.mat"
+path_to_mat_2 = "example/brirs/example_mat.mat"
+tracking_data = scipy.io.loadmat(path_to_mat)
+tracking_data_2 = scipy.io.loadmat(path_to_mat_2)
+
+print()
